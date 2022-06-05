7a5bce6aa9b4036fd926db5f75bade71055e1823
Doni Adrian <doniadrian9@gmail.com>
2022-06-05 19:45:22 +0700

    update file index.js

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (dataNames, sortBy) => {
+  const sortResult = sortBy(dataNames);
+  const result = [];
+  for (let i = 0; i < sortResult.length; i++) {
+      result.push(`${i + 1}. ${sortResult[i]}`);
+  }
+  return result;
+};
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (dataNames) => {
+  return dataNames.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (dataNames) => {
+  return dataNames.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
